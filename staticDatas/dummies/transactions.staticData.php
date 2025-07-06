2ca6e3008872a6352be5848d81dbd078ce49ecd7
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-07-06 20:07:14 +0800

    refactor: add transaction_id and update order_id

--- staticDatas/dummies/transactions.staticData.php
+++ staticDatas/dummies/transactions.staticData.php
@@ -2,8 +2,9 @@
 
 return [
     [
+        'transaction_id' => '2025000',
         'user_id'         => '11111', // Juan Dela Cruz
-        'order_id'        => 'aaa111', // replace with actual order UUID for Juan
+        'order_id'        => 'aaaa-1234', // replace with actual order UUID for Juan
         'transaction_date'=> '2025-07-06 10:10:00',
         'currency'        => 'Zombie Crystal',
         'amount_paid'     => 100.00,
@@ -11,8 +12,9 @@ return [
         'status'          => 'completed',
     ],
     [
+        'transaction_id' => '2025001',
         'user_id'         => '11111', // Juan Dela Cruz
-        'order_id'        => 'aaa222', // replace with another order UUID for Juan
+        'order_id'        => 'aaaa-5678', // replace with another order UUID for Juan
         'transaction_date'=> '2025-07-05 14:40:00',
         'currency'        => 'Zombie Crystal',
         'amount_paid'     => 50.00,
@@ -20,8 +22,9 @@ return [
         'status'          => 'completed',
     ],
     [
+        'transaction_id' => '2025002',
         'user_id'         => '22222', // Admin User
-        'order_id'        => 'bbb111', // replace with actual order UUID for Admin
+        'order_id'        => 'bbbb-1234', // replace with actual order UUID for Admin
         'transaction_date'=> '2025-07-04 09:10:00',
         'currency'        => 'Zombie Crystal',
         'amount_paid'     => 150.00,
