47e3030805005e71852337e6b3964c71e388302d
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-07-06 12:05:55 +0800

    feat: create dummies

---
+++ staticDatas/dummies/users.staticData.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    [
+        'first_name'  => 'Juan',
+        'middle_name' => 'R.',
+        'last_name'   => 'Dela Cruz',
+        'username'    => 'juandelacruz',
+        'password'    => 'zombieSafe99', // will be hashed by the seeder
+        'is_admin'    => false,
+    ],
+    [
+        'first_name'  => 'Admin',
+        'middle_name' => '',
+        'last_name'   => 'User',
+        'username'    => 'admin',
+        'password'    => 'admin123',
+        'is_admin'    => true,
+    ],
+];
